91a300df0daf6452c234c3d8c454c88db3d541ed
SWelbeck <swelbeck12@gmail.com>
2024-09-28 12:48:34 -0400

    pushing up local repo

---
+++ script.js
@@ -0,0 +1,23 @@
+// GLAB 308A.2.1: An Object-Oriented Adventure
+
+// ---------- Part 1: Humble Beginnings ----------
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+};
+// console.log(adventurer.inventory[0])
+// As an additional practice exercise, create a loop that logs each item in Robin’s inventory.
+adventurer.inventory.forEach((el,i)=>{console.log(el)})
+
+// Part 2: ---------- Class Fantasy ----------
+
+// Part 3: ---------- Class Features ----------
+
+// Part 4: ---------- Class Uniforms ----------
+
+// Part 5: ---------- Gather your Party ----------
+
+// Part 6: ---------- Developing Skills ----------
+
+// Part 7: ---------- Adventure Forth ----------
